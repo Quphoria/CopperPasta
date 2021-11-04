ed6200bb4d384474a8c91dc62a2e046237e33c57
Samuel Simpson <s.simpson41.ss@googlemail.com>
2021-11-04 18:11:34 +0000

    handle chunked requests to allow larger requests

--- api/api.py
+++ api/api.py
@@ -64,6 +64,19 @@ def gen_resp(status, data):
     build_cors_headers(response)
     return response
 
+@web_api.before_request
+def handle_chunking():
+    """
+    Sets the "wsgi.input_terminated" environment flag, thus enabling
+    Werkzeug to pass chunked requests as streams; this makes the API
+    compliant with the HTTP/1.1 standard.  The gunicorn server should set
+    the flag, but this feature has not been implemented.
+    """
+
+    transfer_encoding = request.headers.get("Transfer-Encoding", None)
+    if transfer_encoding == "chunked":
+        request.environ["wsgi.input_terminated"] = True
+
 @web_api.route('/', methods=['GET', 'OPTIONS'])
 def api_root():
     resp = _build_cors_preflight_response()
